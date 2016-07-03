6a09c98c4a238350475c30fb837aa880777c1083
SumLare <mrhard95@gmail.com>
2016-07-03 12:40:55 +0300

    Preload routes and provide new forms

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { provideForms } from '@angular/forms';
+import { APP_ROUTER_PROVIDERS } from './app/app.routes';
+
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
-
+bootstrap(AppComponent, [
+  provideForms(), APP_ROUTER_PROVIDERS
+])
