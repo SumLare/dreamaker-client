4e53372e3c8bfdf94e3ac11e431b57e89cffc86a
SumLare <mrhard95@gmail.com>
2016-07-05 15:40:07 +0300

    Change default AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { GoalService } from './+goals/shared/index';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <router-outlet></router-outlet>
+    `,
+  styleUrls: ['app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [HTTP_PROVIDERS, GoalService]
 })
+
 export class AppComponent {
-  title = 'app works!';
+
 }
