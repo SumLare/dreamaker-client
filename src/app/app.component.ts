e948cab8c4ac335466aa282180857a53cd9cfe65
SumLare <mrhard95@gmail.com>
2016-07-07 04:03:44 +0300

    Toolbar with menu icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,24 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { GoalService } from './+goals/shared/index';
 
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-    `,
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  providers: [HTTP_PROVIDERS, GoalService]
+  directives: [
+    ROUTER_DIRECTIVES,
+    MdToolbar,
+    MdIcon
+  ],
+  providers: [
+    HTTP_PROVIDERS,
+    GoalService,
+    MdIconRegistry
+  ]
 })
 
 export class AppComponent {
